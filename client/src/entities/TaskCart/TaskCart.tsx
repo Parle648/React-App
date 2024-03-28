4fa5145fcca446d3e16529e58ae151fcfa0aebbd
Parle648 <sashahoumi@gmail.com>
2024-03-28 20:12:59 +0200

    Create task modal basic UI and toggleModal logic

--- client/src/entities/TaskCart/TaskCart.tsx
+++ client/src/entities/TaskCart/TaskCart.tsx
@@ -6,6 +6,7 @@ import { TaskCartProps } from './types/taskCartProps';
 import ChangeModal from '../CgangeModal/ChangeModal';
 import DeleteTaskFeature from '../../features/DeleteTaskFeature/DeleteListFeature';
 import ChangeTaskInfo from '../../features/ChangeTaskInfo/ChangeTaskInfo';
+import { useState } from 'react';
 
 const TaskCart = ({
     priority, 
@@ -15,8 +16,15 @@ const TaskCart = ({
     id,
     listName }: TaskCartProps) => {
     const date = new Date(deadline).toLocaleDateString();
+
+    const [visible, setVisible] = useState<boolean>(false);
+
+    function toggleModal() {
+        setVisible(!visible)
+    }
+
     return (
-        <div className={styles.cart}>
+        <div className={styles.cart} onClick={toggleModal}>
             <ChangeModal>
                 <DeleteTaskFeature task_id={id} task_name={name} />
                 <ChangeTaskInfo id={id} name={name} deadline={deadline} descryption={description} priority={priority} />
@@ -31,6 +39,20 @@ const TaskCart = ({
             </p>
             <PriorityBlock priority={priority} />
             <MoveTo list_name={listName} task_id={id} task_name={name} />
+            <div className={`${styles.modalContainer} ${visible && styles.visible}`}>
+                <div className={styles.modalBlock}>
+                    <button onClick={toggleModal}>X</button>
+                    <h2 className={styles.taskName}>{name}</h2>
+                    <p className={styles.description}>
+                        {description}
+                    </p>
+                    <p className={styles.deadline}>
+                        <img src={calendar} alt="calendar-icon" />
+                        {date}
+                    </p>
+                    <PriorityBlock priority={priority} />
+                </div>
+            </div>
         </div>
     );
 };
