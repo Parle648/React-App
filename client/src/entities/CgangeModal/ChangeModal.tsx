fd94a6f2aa78ac94355575b9690c3952bcb71a91
Parle648 <sashahoumi@gmail.com>
2024-03-29 15:59:01 +0200

    Refactoring ChangeModal entity
    *Changes:
     - Replase useState using useToggle
     - Create ChangeModalProps type
    
    *Branch:
     - first-stage

--- client/src/entities/CgangeModal/ChangeModal.tsx
+++ client/src/entities/CgangeModal/ChangeModal.tsx
@@ -1,18 +1,14 @@
-import React, { useState } from 'react';
 import styles from './styles/changeModal.module.scss';
 import menu from '../../shared/assets/img/menu-vertical.png';
+import useToggle from '../../shared/lib/hooks/useToggle';
+import { ChangeModalProps } from './types/changeModalProps';
 
-const ChangeModal = ({children}: {children: any}) => {
-    const [visible, setVisible] = useState<boolean>(false)
-
-    function toggleModal(event: any) {
-        event.stopPropagation()
-        setVisible(!visible)
-    }
+const ChangeModal = ({children}: ChangeModalProps) => {
+    const [visible, setVisible] = useToggle(false)
 
     return (
         <div className={styles.block}>
-            <button className={styles.open} onClick={toggleModal}>
+            <button className={styles.open} onClick={() => setVisible()}>
                 <img className={styles.img} src={menu} alt="menu" />
             </button>
             <div className={`${styles.modal} ${visible && styles.modalActive}`}>
